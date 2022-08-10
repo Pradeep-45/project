b60851edccd9af62a3c08f9b4c081803e6aef9d0
Pradeep-45 <Pradeepsanapala456@gmail.com>
2022-08-10 15:38:40 +0530

    first commit

---
+++ Phase4Section1.4/GoogleSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Phase4Section1._4
+{
+    [TestClass]
+    public class GoogleSearch
+    {
+        [TestMethod]
+        public void FFSearch()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://www.google.com");
+
+            }
+        }
+    }
+}
---
+++ Phase4Section1.4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Phase4Section1.4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Phase4Section1.4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a85dc969-ba85-41ef-81a4-5ef92738bf83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
